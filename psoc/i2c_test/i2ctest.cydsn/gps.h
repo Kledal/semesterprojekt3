71ba3d66cb3b4b49e95c8f1f65e5d5c46edd0f10
Kledal <alex.kledal@gmail.com>
2014-11-26 15:35:34 +0100

    Cleaned up and added gps header

--- psoc/i2c_test/i2ctest.cydsn/gps.h
+++ psoc/i2c_test/i2ctest.cydsn/gps.h
@@ -13,3 +13,22 @@ char GetGPSLongitudePos();
 
 void GetGPSLatitude(char * returnStr);
 void GetGPSLongitude(char * returnStr);
+
+/********** GPS  ***************/
+
+char gpsBuffer[300];
+int gpsBufferPointer;
+char* gpvtg;
+char* gpgga;
+char* gpgsv;
+
+int speed_;
+int numberOfSate_;
+int time_;
+
+float longitude_;
+float latitude_;
+char latitude_pos;
+char longitude_pos;
+
+/********** /GPS  ***************/
