91e31c82dbd5f6f18e8b371f62a02aa749a2121f
Lasse Thostrup <201271288@post.au.dk>
2014-11-28 12:32:39 +0100

    Added folder structure to devkit-kode

--- PSoC_SPI_Driver/.psocmod.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/stud/i3hal_e2014_a/src/Exercise_7/PSoC_SPI_Driver/psocmod.o := arm-angstrom-linux-gnueabi-ld -EL    -r -o /home/stud/i3hal_e2014_a/src/Exercise_7/PSoC_SPI_Driver/psocmod.o /home/stud/i3hal_e2014_a/src/Exercise_7/PSoC_SPI_Driver/psoc.o /home/stud/i3hal_e2014_a/src/Exercise_7/PSoC_SPI_Driver/psoc-spi.o 
