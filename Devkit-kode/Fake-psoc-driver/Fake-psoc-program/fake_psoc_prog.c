dc93d9e82866d733aee872ae70edb16ff5df614f
Lasse Thostrup <201271288@post.au.dk>
2014-11-28 14:34:35 +0100

    Added Fakies and fake program for the psoc driver

---
+++ Devkit-kode/Fake-psoc-driver/Fake-psoc-program/fake_psoc_prog.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main()
+{
+  int fd_speed, fd_bat;
+  char buffer_speed[6], buffer_bat[6];
+  fd_speed = open("/sys/class/fake_psoc/fake_psoc1/speed", O_WRONLY);
+  fd_bat = open("/sys/class/fake_psoc/fake_psoc1/speed", O_WRONLY);
+  int count = 0;
+  int len_speed, len_bat;
+  while(1)
+  {
+  	count++;
+  	len_speed = sprintf(buffer_speed, "%d", count);
+  	len_bat = sprintf(buffer_bat, "%d", count);
+
+    if(!write(fd_speed, buffer_speed, len_speed))
+    printf("ERROR write to speed");
+
+    if(!write(fd_bat, buffer_bat, len_bat))
+    printf("ERROR write to battery");
+
+  	usleep(500000); 	//Sleep 0.5 sec
+    
+  }
+return 0;
+}
